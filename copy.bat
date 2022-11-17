3c438d2e20258dcb89f1dfe4d32c2705a884f603
Cameron Griffin (He/Him) <101752687+CodrCam@users.noreply.github.com>
2022-11-17 09:24:06 -0800

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,2 +1,11 @@
+#Script: Ops 201 Class 08 Ops Challenge Solution
+#Author: Cameron Griffin
+#Date of latest revision: 11-17-2022
+#Purpose: Creata an array and creates files in array
+
+
+# robocopy bat file that transfers from one place to another.
+
+
 robocopy "C:\User\j.thompson\" "C:\User\j.thompson\Desktop\My Project Files\"
 pause
